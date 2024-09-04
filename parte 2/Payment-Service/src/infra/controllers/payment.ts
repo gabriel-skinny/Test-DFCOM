2efc6235c6424d7888d57c5d3a788cf37c889eb0
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-09-04 16:37:00 -0300

    Making microservice and adding new route

--- parte 2/Payment-Service/src/infra/controllers/payment.ts
+++ parte 2/Payment-Service/src/infra/controllers/payment.ts
@@ -3,22 +3,27 @@ import { MessagePattern } from '@nestjs/microservices';
 import { CreatePaymentUseCase } from 'src/application/use-cases/create';
 import { WebhookPaymentConfirmation } from 'src/application/use-cases/webhook-payment-confirmation';
 
-@Controller('payment')
+interface ICreatePaymentParams {
+  orderId: string;
+  userId: string;
+  creditCardNumber: string;
+  creditCardSecurityNumber: string;
+  creditCardExpirationDate: string;
+  value: number;
+}
+
+@Controller()
 export class PaymentController {
   constructor(
     private readonly createPaymentUseCase: CreatePaymentUseCase,
     private readonly webhookPaymentConfirmationUseCase: WebhookPaymentConfirmation,
   ) {}
 
-  async create(data: {
-    orderId: string;
-    userId: string;
-    creditCardNumber: string;
-    creditCardSecurityNumber: string;
-    creditCardExpirationDate: string;
-    value: number;
-  }) {
-    await this.createPaymentUseCase.execute(data);
+  @MessagePattern({ cmd: 'create-payment' })
+  async create(data: ICreatePaymentParams): Promise<{ paymentId: string }> {
+    const { paymentId } = await this.createPaymentUseCase.execute(data);
+
+    return { paymentId };
   }
 
   @MessagePattern({ cmd: 'webhook-payment-confirmation' })
--- parte 2/Payment-Service/src/main.ts
+++ parte 2/Payment-Service/src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: process.env.SERVICE_HOST,
+        port: Number(process.env.SERVICE_PORT),
+      },
+    },
+  );
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(6000);
+  await app.listen();
 }
 bootstrap();
