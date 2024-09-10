03142c1a56964f6f3f5f128d7b04fa63f52f6bc1
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-09-10 15:26:29 -0300

    Adding new fields to event

--- parte 2/Event-Service/src/infra/database/entities/event.ts
+++ parte 2/Event-Service/src/infra/database/entities/event.ts
@@ -1,9 +1,9 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
 
 export type EventDocument = HydratedDocument<EventModel>;
 
-@Schema({ collection: 'event' })
+@Schema({ collection: "event" })
 export class EventModel {
   constructor(eventModel: EventDocument) {
     Object.keys(eventModel).map((key) => (this[key] = eventModel[key]));
@@ -24,13 +24,23 @@ export class EventModel {
   ticketNumber: number;
 
   @Prop()
+  availableTickets: number;
+
+  @Prop()
+  creatorId: string;
+
+  @Prop()
   publishedDate: Date;
 
   @Prop()
   endSellingDate: Date;
 
   @Prop()
+  available: boolean;
+
+  @Prop()
   createdAt: Date;
+
   @Prop()
   updatedAt?: Date;
 
